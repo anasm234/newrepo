4fd65fa12595c5b53de921fba1f536181f68f48c
anasm234 <anas.m234@gmail.com>
2015-12-14 17:13:21 +0530

    5.10 from server

--- anas.php
+++ anas.php
@@ -6,4 +6,6 @@ echo "from Chennai, Tamil Nadu";
 echo "Currently Working with Colan Info Tech";
 echo "Before worked with Quatris technology";
 echo "Added from git server";
+
+echo "From server 5.10"
 ?>
